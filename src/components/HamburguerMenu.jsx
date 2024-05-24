c1285244320df501cd9d1ad9362a65fed23a8e96
prltagustina <prltagustina@gmail.com>
2024-05-24 13:26:52 -0300

    values menu

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -35,10 +35,10 @@ function HamburguerMenu() {
               <a href="#about">Sobre Nosotros</a>
             </li>
             <li>
-              <a href="#services">Servicios</a>
+              <a href="#values">Valores</a>
             </li>
             <li>
-              <a href="#values">Valores</a>
+              <a href="#services">Servicios</a>
             </li>
             <li>
               <a href="#contact">Contacto</a>
