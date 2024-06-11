883e3937ccd4e2297aacbdda7342040369611940
prltagustina <prltagustina@gmail.com>
2024-06-11 16:55:36 -0300

    a href attributes update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,16 +19,36 @@ function Footer() {
       {/* Cambiar a un tono de gris oscuro */}
       <div className="container mx-auto px-4">
         <div className="flex justify-center space-x-6 items-center">
-          <motion.a href="tel:1234567890" {...iconHoverAnimation}>
+          <motion.a
+            href="tel:1234567890"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaPhone size={24} />
           </motion.a>
-          <motion.a href="#" {...iconHoverAnimation}>
+          <motion.a
+            href="https://www.google.com/maps/place/Santa+Fe,+Santa+Fe+Province,+Argentina"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaMapMarkerAlt size={24} />
           </motion.a>
-          <motion.a href="https://www.linkedin.com/" {...iconHoverAnimation}>
+          <motion.a
+            href="https://www.linkedin.com/in/santiagoperaltarrhh/"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin size={24} />
           </motion.a>
-          <motion.a href="mailto:info@example.com" {...iconHoverAnimation}>
+          <motion.a
+            href="mailto:consultorapermol@outlook.com"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaEnvelope size={24} />
           </motion.a>
         </div>
