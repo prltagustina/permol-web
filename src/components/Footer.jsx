7e636cad11fc7860e535566e89b837751f4c9c02
prltagustina <prltagustina@gmail.com>
2024-06-20 15:11:39 -0300

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,36 +5,19 @@ import {
   FaMapMarkerAlt,
   FaLinkedin,
   FaEnvelope,
+  FaCode, // Importa el ícono necesario para "Desarrollado por"
 } from "react-icons/fa";
 
 const iconHoverAnimation = {
-  whileHover: { scale: 1.1, color: "#5472d3" }, // Cambiar el color a un tono azulado
+  whileHover: { scale: 1.1, color: "#5472d3" },
   transition: { duration: 0.2 },
 };
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-8">
-      {" "}
-      {/* Cambiar a un tono de gris oscuro */}
       <div className="container mx-auto px-4">
         <div className="flex justify-center space-x-6 items-center">
-          <motion.a
-            href="tel:1234567890"
-            {...iconHoverAnimation}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaPhone size={24} />
-          </motion.a>
-          <motion.a
-            href="https://www.google.com/maps/place/Santa+Fe,+Santa+Fe+Province,+Argentina"
-            {...iconHoverAnimation}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaMapMarkerAlt size={24} />
-          </motion.a>
           <motion.a
             href="https://www.linkedin.com/in/santiagoperaltarrhh/"
             {...iconHoverAnimation}
@@ -51,9 +34,39 @@ function Footer() {
           >
             <FaEnvelope size={24} />
           </motion.a>
+          <motion.a
+            href="https://www.google.com/maps/place/Santa+Fe,+Santa+Fe+Province,+Argentina"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaMapMarkerAlt size={24} />
+          </motion.a>
+          <motion.a
+            href="tel:5493424085669"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaPhone size={24} />
+          </motion.a>
         </div>
+        {/* Texto de derechos reservados */}
         <div className="mt-4 text-center">
           <p>© 2024 Consultora Permol. Todos los derechos reservados.</p>
+        </div>
+        {/* Contenedor para "Desarrollado por" */}
+        <div className="flex justify-center items-center mt-2">
+          <span className="text-sm mr-1">Desarrollado por</span> {/* Texto "Desarrollado por" */}
+          <motion.a
+            href="https://agustina-peralta.web.app/"
+            {...iconHoverAnimation}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <FaCode size={24} /> {/* Ícono ficticio */}
+          </motion.a>
         </div>
       </div>
     </footer>
