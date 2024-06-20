7891f092e60b7b6476f8b0aabeca0506d2ddf54f
prltagustina <prltagustina@gmail.com>
2024-06-20 15:54:13 -0300

    handleclick hamburguerMenu

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 function HamburguerMenu({ menuColor }) {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null); // Referencia al menú hamburguesa
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const closeMenu = (event) => {
+      // Verificar si se hizo clic fuera del menú hamburguesa
+      if (isOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    // Agregar un event listener para detectar clics en cualquier parte de la página
+    document.addEventListener("click", closeMenu);
+
+    // Limpiar el event listener cuando el componente se desmonta o el estado cambia
+    return () => {
+      document.removeEventListener("click", closeMenu);
+    };
+  }, [isOpen]); // Asegurarse de que el efecto se ejecute solo cuando el estado isOpen cambia
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Cerrar el menú al hacer clic en cualquier enlace del menú desplegable
+  };
+
   return (
-    <div className="relative">
+    <div className="relative" ref={menuRef}>
       <button
         onClick={toggleMenu}
         className={`text-white md:hidden ${menuColor}`} // Clase dinámica para el color del menú
@@ -48,12 +70,14 @@ function HamburguerMenu({ menuColor }) {
           >
             {/* Agregar icono de cierre (X) si lo deseas */}
           </button>
-          <ul className="space-y-2"> {/* Espaciado vertical entre elementos */}
+          <ul className="space-y-2">
+            {/* Espaciado vertical entre elementos */}
             <li>
               <motion.a
                 href="#about"
                 className="hover:bg-blue-600 hover:text-white text-lg py-3 px-4 block rounded-md text-center"
                 whileTap={{ scale: 0.95 }} // Efecto de escala cuando se presiona el botón
+                onClick={handleLinkClick} // Manejar clic en el enlace para cerrar el menú
               >
                 Sobre Nosotros
               </motion.a>
@@ -63,6 +87,7 @@ function HamburguerMenu({ menuColor }) {
                 href="#values"
                 className="hover:bg-blue-600 hover:text-white text-lg py-3 px-4 block rounded-md text-center"
                 whileTap={{ scale: 0.95 }} // Efecto de escala cuando se presiona el botón
+                onClick={handleLinkClick} // Manejar clic en el enlace para cerrar el menú
               >
                 Valores
               </motion.a>
@@ -72,6 +97,7 @@ function HamburguerMenu({ menuColor }) {
                 href="#services"
                 className="hover:bg-blue-600 hover:text-white text-lg py-3 px-4 block rounded-md text-center"
                 whileTap={{ scale: 0.95 }} // Efecto de escala cuando se presiona el botón
+                onClick={handleLinkClick} // Manejar clic en el enlace para cerrar el menú
               >
                 Servicios
               </motion.a>
@@ -81,6 +107,7 @@ function HamburguerMenu({ menuColor }) {
                 href="#contact"
                 className="hover:bg-blue-600 hover:text-white text-lg py-3 px-4 block rounded-md text-center"
                 whileTap={{ scale: 0.95 }} // Efecto de escala cuando se presiona el botón
+                onClick={handleLinkClick} // Manejar clic en el enlace para cerrar el menú
               >
                 Contacto
               </motion.a>
